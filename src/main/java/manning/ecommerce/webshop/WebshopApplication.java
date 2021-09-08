7b8fb2d7a0e13209df9a20d6fc3464390f199a12
RobbyQuintiensPXL <robby.quintiens@student.pxl.be>
2021-09-08 10:45:43 +0200

    added RestControllerTests added HttpServlet for testing

--- src/main/java/manning/ecommerce/webshop/WebshopApplication.java
+++ src/main/java/manning/ecommerce/webshop/WebshopApplication.java
@@ -2,14 +2,14 @@ package manning.ecommerce.webshop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
 @SpringBootApplication
+@ServletComponentScan
 public class WebshopApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(WebshopApplication.class, args);
-        String message = "Hello world!";
-        System.out.println(message.substring(6,12) + message.substring(12,6));
     }
 
 }
