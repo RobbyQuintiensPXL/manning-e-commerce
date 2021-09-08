3067f0785c85f2b0b8cbe66512438952a5271aea
RobbyQuintiensPXL <robby.quintiens@student.pxl.be>
2021-09-08 08:10:57 +0200

    CakeShop

--- src/main/java/manning/ecommerce/webshop/WebshopApplication.java
+++ src/main/java/manning/ecommerce/webshop/WebshopApplication.java
@@ -8,6 +8,8 @@ public class WebshopApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(WebshopApplication.class, args);
+        String message = "Hello world!";
+        System.out.println(message.substring(6,12) + message.substring(12,6));
     }
 
 }
--- src/main/java/manning/ecommerce/webshop/controller/HomeController.java
+++ src/main/java/manning/ecommerce/webshop/controller/HomeController.java
@@ -1,12 +1,8 @@
-package manning.ecommerce.webshop.controllers;
+package manning.ecommerce.webshop.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Controller
 public class HomeController {
